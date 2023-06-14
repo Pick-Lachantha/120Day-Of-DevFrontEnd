323b3ae69921bfec157018296bb1b7f9f4c9311c
Bishop <mhabig1045@gmail.com>
2023-06-14 20:42:20 +0700

    Update

--- Day4 Tabs/script.js
+++ Day4 Tabs/script.js
@@ -10,8 +10,8 @@ function openCity(evt, cityName){
 
     //Get all element with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks")
-    for (i = 0; i < tablinks.length; i++){
-        tablinks[i].className = tablinks[i].className.replace("active","")
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
     //Show the current tab and add an "active" class to the button that opened the tab
